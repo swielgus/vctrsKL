bfdcd9a0c620a1368466b747d5dead612913a84e
swielgus <szymonwiel@gmail.com>
2017-01-12 15:15:04 +0100

    Added methods of loading image & getting pixel color info to ImageData

--- src/ImageData.hpp
+++ src/ImageData.hpp
@@ -11,10 +11,19 @@
 class ImageData
 {
 public:
+    using color_type = png::byte;
+
     std::size_t getWidth() const;
     std::size_t getHeight() const;
+
+    void loadImage(std::string filename);
+    const color_type& getPixelRed(std::size_t x, std::size_t y) const;
+    const color_type& getPixelGreen(std::size_t x, std::size_t y) const;
+    const color_type& getPixelBlue(std::size_t x, std::size_t y) const;
 private:
     png::image<png::rgb_pixel> internalImage;
+
+    const png::rgb_pixel& getPixel(std::size_t x, std::size_t y) const;
 };
 
 #endif //VCTRSKL_IMAGEDATA_HPP
--- test/ImageDataTest.cpp
+++ test/ImageDataTest.cpp
@@ -10,15 +10,44 @@ struct ImageDataTest : testing::Test
     ImageData testedData;
 
     ImageDataTest()
-        : testedData()
-    { }
+            : testedData()
+    {}
 
     virtual ~ImageDataTest()
-    { }
+    {}
 };
 
 TEST_F(ImageDataTest, defaultlyConstructedClassShouldHaveNoImage)
 {
     EXPECT_EQ(0U, testedData.getWidth());
     EXPECT_EQ(0U, testedData.getHeight());
+}
+
+TEST_F(ImageDataTest, zigzagImageShouldBeLoadedAndVerified)
+{
+    testedData.loadImage("images/chk_2x20.png");
+    unsigned int expectedWidth = 20;
+    unsigned int extectedHeight = 2;
+
+    EXPECT_EQ(expectedWidth, testedData.getWidth());
+    EXPECT_EQ(extectedHeight, testedData.getHeight());
+
+    for(std::size_t i = 0; i < expectedWidth/2; i+=2)
+    {
+        EXPECT_EQ(255 * (i % 2), testedData.getPixelRed(0, i));
+        EXPECT_EQ(255 * (i % 2), testedData.getPixelGreen(0, i));
+        EXPECT_EQ(255 * (i % 2), testedData.getPixelBlue(0, i));
+
+        EXPECT_EQ(255 * !(i % 2), testedData.getPixelRed(1, i));
+        EXPECT_EQ(255 * !(i % 2), testedData.getPixelGreen(1, i));
+        EXPECT_EQ(255 * !(i % 2), testedData.getPixelBlue(1, i));
+
+        EXPECT_EQ(255 * !(i % 2), testedData.getPixelRed(0, i+1));
+        EXPECT_EQ(255 * !(i % 2), testedData.getPixelGreen(0, i+1));
+        EXPECT_EQ(255 * !(i % 2), testedData.getPixelBlue(0, i+1));
+
+        EXPECT_EQ(255 * (i % 2), testedData.getPixelRed(1, i+1));
+        EXPECT_EQ(255 * (i % 2), testedData.getPixelGreen(1, i+1));
+        EXPECT_EQ(255 * (i % 2), testedData.getPixelBlue(1, i+1));
+    }
 }
