495301d5847086e34f7c1da2faee4cbb323ff819
swielgus <szymonwiel@gmail.com>
2017-01-16 12:23:22 +0100

    Added more test cases for PixelGraph

--- test/PixelGraphTest.cpp
+++ test/PixelGraphTest.cpp
@@ -17,12 +17,48 @@ struct PixelGraphTest : testing::Test
     }
 };
 
+TEST_F(PixelGraphTest, shouldConstruct1x1Graph)
+{
+    ImageData testedImage("images/1x1.png");
+    testedGraph = new PixelGraph(testedImage);
+
+    std::vector<std::vector<color_type>> expectedResult{ {0} };
+
+    EXPECT_EQ(expectedResult, testedGraph->getEdgeValues());
+}
+
 TEST_F(PixelGraphTest, shouldConstructTwoPointGraphWithNoEdges)
 {
     ImageData testedImage("images/chk_1x2.png");
     testedGraph = new PixelGraph(testedImage);
 
     std::vector<std::vector<color_type>> expectedResult{ {0, 0} };
+
+    EXPECT_EQ(expectedResult, testedGraph->getEdgeValues());
+}
+
+TEST_F(PixelGraphTest, shouldConstructHorizontalLineGraph)
+{
+    ImageData testedImage("images/1x100.png");
+    testedGraph = new PixelGraph(testedImage);
+
+    std::vector<std::vector<color_type>> expectedResult{ {32} };
+    for(int i = 1; i <= 98; ++i)
+        expectedResult[0].push_back(34);
+    expectedResult[0].push_back(2);
+
+    EXPECT_EQ(expectedResult, testedGraph->getEdgeValues());
+}
+
+TEST_F(PixelGraphTest, shouldConstructVerticalLineGraph)
+{
+    ImageData testedImage("images/100x1.png");
+    testedGraph = new PixelGraph(testedImage);
+
+    std::vector<std::vector<color_type>> expectedResult{ {8} };
+    for(int i = 1; i <= 98; ++i)
+        expectedResult.push_back({136});
+    expectedResult.push_back({128});
 
     EXPECT_EQ(expectedResult, testedGraph->getEdgeValues());
 }
@@ -45,6 +81,19 @@ TEST_F(PixelGraphTest, shouldConstructAGraphRepresentingA20Zigzag)
     std::vector<std::vector<color_type>> expectedResult{
             {16, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 4},
             {64, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 1}};
+
+    EXPECT_EQ(expectedResult, testedGraph->getEdgeValues());
+}
+
+TEST_F(PixelGraphTest, shouldConstructAGraphRepresentingA20VerticalZigzag)
+{
+    ImageData testedImage("images/chk_20x2.png");
+    testedGraph = new PixelGraph(testedImage);
+
+    std::vector<std::vector<color_type>> expectedResult{ {16,4} };
+    for(int i = 1; i <= 18; ++i)
+        expectedResult.push_back({80, 5});
+    expectedResult.push_back({64,1});
 
     EXPECT_EQ(expectedResult, testedGraph->getEdgeValues());
 }
